f913d3bb4ff99a0d762944e4effc9c43dcdc388b
avacadokkk <ypc71469@gmail.com>
2025-03-14 13:56:53 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Contain from "../components/Containontain";
+import Contain from "../components/contain";
 function Home() {
     return (
         <>
